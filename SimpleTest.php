1b9ac25f0ba9c6e407d2d9c5a4ebc23a3a4100e6
nmuhammadfadli <124648757+nmuhammadfadli@users.noreply.github.com>
2023-10-21 14:07:17 +0700

    test

--- SimpleTest.php
+++ SimpleTest.php
@@ -1,16 +1,23 @@
 <?php
-use PHPUnit\Framework\TestCase;
-require_once 'D:\Xampp\htdocs\PhpTesting\Wordcount.php';
+use PHPUnit\Framework\TestCase; //pernyataan untuk mengimpor kelas 
+//TestCase adalah kelas dasar yang digunakan untuk membuat unit tes dalam PHPUnit
+require_once 'D:\Xampp\htdocs\PhpTesting\Wordcount.php'; //pernyataan yang digunakan untuk memuat file Wordcount.php yang terletak di jalur yang ditentukan
+// require_once digunakan untuk memastikan bahwa file hanya dimuat sekali, jika sudah dimuat sebelumnya, maka tidak akan dimuat lagi
 
-class SimpleTest extends TestCase
+class SimpleTest extends TestCase //mendefinisikan kelas SimpleTest yang merupakan subkelas dari TestCase
+//Kelas ini akan digunakan untuk menulis dan menjalankan tes unit
 {
-  public function testcountWords(){
-    $Wc = new Wordcount();
+  public function testcountWords(){ //metode yang mendefinisikan tes unit dengan nama testcountWords()
+    //Testing ini dibuat untuk menghitung jumlah kata dalam kalimat yang diberikan
+    $Wc = new Wordcount();//membuat instance objek dari kelas Wordcount dan menyimpannya dalam variabel $Wc
+    //Kelas Wordcount  berisi metode countWords() yang akan diuji
 
-    $TestSentence = "My Name is Joko";
-    $Wordcount = $Wc->countWords($TestSentence);
-
+    $TestSentence = "My Name is Joko"; //memberikan definisi variabel $TestSentence dan menginisialisasinya dengan kalimat yang akan diuji
+    $Wordcount = $Wc->countWords($TestSentence); //: Ini memanggil metode countWords() pada objek $Wc dengan parameter $TestSentence
+    // method ini akan menghitung jumlah kata dalam kalimat dan hasilnya akan disimpan dalam variabel $Wordcount.
+    
     $this->assertEquals(4, $Wordcount); 
+    //i adalah pernyataan pengujian yang membandingkan hasil yang diharapkan (4) dengan hasil aktual yang dihitung oleh metode countWords()
   }  
 }
 ?>
